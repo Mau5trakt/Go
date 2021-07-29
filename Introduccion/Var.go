1799d97448bbbf17b5ed906d1727f97cf28b7fd9
Mau5trakt <kubzalemany@hotmail.com>
2021-07-29 11:02:49 -0600

    Var declaration

---
+++ Introduccion/Var.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+var z = 41
+
+func main() {
+	x := 42 + 7
+	y := "James Bond"
+	fmt.Println(x)
+	fmt.Println(y)
+	numero()
+}
+
+func numero() {
+	fmt.Println("Bueno esto es la funcion numero pero la tuve que llamar en main xd")
+	fmt.Println(z)
+}
