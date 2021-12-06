dbb705f6dba1acd7dfb4df2c28a7bd7ac2b8dcbe
Mau5trakt <kubzalemany@hotmail.com>
2021-12-06 00:03:34 -0600

    workspaces

---
+++ Introduccion/fmt package.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+var a int
+var b string = "Programa"
+func main()  {
+	fmt.Println(a)
+	fmt.Println(b)
+	//Esto es como el print(f"") de python
+	fmt.Printf("El valor de la variable a es: %v", a )
+
+}
