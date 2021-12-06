63a82b329df6004d16d34e8e8cc8320a415bf899
Mau5trakt <kubzalemany@hotmail.com>
2021-12-06 00:04:10 -0600

    Type of variables in GoLang

--- Introduccion/Var Type.go
+++ Introduccion/Var Type.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 )
-
-var z int = 21
+//Declarando la variable con el identificador z de tipo int
+var z int
 
 func main() {
+	z = 21
 	fmt.Print("La edad minima para estar aqui es de: ", z)
 
 }
